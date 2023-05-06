baedd71a3e9f7f586acefb02ae61bb193c453a80
Frank Lemanschik <117739566+lemanschik@users.noreply.github.com>
2023-05-06 10:26:34 +0200

    Create deps.js

---
+++ packages/vite/modules/deps.js
@@ -0,0 +1,7 @@
+/* Updates Generates Deps */
+// As deps is a npm bound process this got added as extra module 
+// unlike the other modules in this directory excluding deps
+// this also requires npm and node as the deps are not in shape
+// We need to do a lot of processing to get valid source code.
+// This process should only be needed every 2 month so 6 times a year hopefully
+// We will try to add a addition deps-nightly.js that can export the deps dynamicaly and cached. 
